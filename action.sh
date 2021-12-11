9ce4519f3ec7e9942d6bb48d653a0015c9ca9aa2
Kuhn Chris <kuhnchris+github@kuhnchris.eu>
2021-12-11 20:32:14 +0100

    Changing

--- action.sh
+++ action.sh
@@ -1,90 +1,10 @@
-#!/bin/bash
-set -v -x
-echo "::group::Debugging information:"
-echo "workdir:"
-pwd
-echo "os info:"
-uname -a
-echo "Godot executable lib dependencies"
-ldd /usr/bin/godot
-echo "current workdir listing:"
-ls -alh
-echo "command line:"
-echo "$@"
-echo "::endgroup::"
+#!/bin/sh
+#set -v -x
+TAG=gh-export-godot-project
 
-echo "::group::Parsing and preparing variables"
-
-targetDir=${GITHUB_WORKSPACE}
-BASE_DIR=$1
-DEBUG=$2
-PACK=$3
-PLATFORM_EXPORT=$4
-PLATFORM="$5"
-EXECNAME=$6
-if [ "${BASE_DIR}x" != "x" ] && [ "$BASE_DIR" != "false" ]; then
-    targetDir="${GITHUB_WORKSPACE}/${BASE_DIR}/"
-    echo "Using this project directory: ${targetDir}"
-fi
-sanitizePlatform=$(echo "${PLATFORM}" | sed -e 's/[^A-Za-z0-9._-]/_/g')
-localExportDirBase=.ci-exports/${sanitizePlatform}/
-localTargetDirDebug=${localExportDirBase}/export-debug
-localTargetDirPck=${localExportDirBase}/export-pck
-localTargetDirPlatform=${localExportDirBase}/export-platform
-targetDirDebug=${targetDir}/${localTargetDirDebug}/
-targetDirPck=${targetDir}/${localTargetDirPck}/
-targetDirPlatform=${targetDir}/${localTargetDirPlatform}/
-projectFile=${targetDir}/project.godot
-exportPresetFile=${targetDir}/export_presets.cfg
-logfile="export-artifacts/engine-output-${sanitizePlatform}.log"
-
-echo "::endgroup::"
-
-
-echo "::group::Linking template directory to proper directory"
-sharedir=~/.local/share/
-mkdir -pv ${sharedir}
-ln -sv /usr/share/godot ${sharedir}/godot
-echo "::endgroup::"
-
-echo "::group::Cleaning and preparing export directories..."
-rm -Rfv "${targetDirDebug}" 2>&1 || true
-mkdir -pv "${targetDirDebug}"
-rm -Rfv "${targetDirPck}" 2>&1 || true
-mkdir -pv "${targetDirPck}"
-rm -Rfv "${targetDirPlatform}" 2>&1 || true
-mkdir -pv "${targetDirPlatform}"
-#rm -Rf ./export-artifacts 2>&1 || true
-mkdir ./export-artifacts
-echo "::endgroup::"
-
-echo "::group::Evaluating input variables..."
-sed "s[name=\"$PLATFORM\"[name=\"${sanitizePlatform}\"[g" -i "${exportPresetFile}" 
-godot_args=("--no-window" "${projectFile}" "--quit")
-ziping=""
-zippostfix="$(date "+automated_build-%Y.%m.%d-%H%M%S")-$GITHUB_SHA"
-if [ "${DEBUG}x" != "x" ] && [ "${DEBUG}x" != "falsex" ]; then
-    godot_args+=("--export-debug" "${sanitizePlatform}" "${localTargetDirDebug}/${EXECNAME}")
-    ziping="zip -0 -r -D \"export-artifacts/build-${sanitizePlatform}-export-with-debug-symbols-${zippostfix}.zip\" ${targetDirDebug} ;"
-fi
-
-if [ "${PACK}x" != "x" ] && [ "${PACK}x" != "falsex" ]; then
-    godot_args+=("--export-pack" "${sanitizePlatform}" "${localTargetDirPck}/${EXECNAME}")
-    ziping="${ziping}zip -0 -r -D \"export-artifacts/build-${sanitizePlatform}-export-pack-${zippostfix}.zip\" ${targetDirPck} ;"
-fi
-
-if [ "${PLATFORM_EXPORT}x" != "x" ] && [ "${PLATFORM_EXPORT}x" != "falsex" ]; then
-    godot_args+=("--export" "${sanitizePlatform}" "${localTargetDirPlatform}/${EXECNAME}")
-    ziping="${ziping}zip -0 -r -D \"export-artifacts/build-${sanitizePlatform}-export-${zippostfix}.zip\" ${targetDirPlatform} ;"
-fi
-echo "::endgroup::"
-
-execs=(/usr/bin/godot)
-chmod +x "${execs[0]}"
-echo "::group::running the engine with following parameters: ${godot_args[*]}"
-eval "${execs[0]}" ${godot_args[*]} > "${logfile}" 2>&1 
-echo "::endgroup::"
-echo "::group::ziping projects..."
-eval "${ziping}" 2>&1 
-echo "::endgroup::"
-set +v +x
+cd "${ACTION_PATH}" || exit 2;
+sed 's/GODOT_VERSION/'${GODOT_VERSION}'/g' -i docker/entrypoint.sh
+cd docker || exit 2;
+docker build . --file Dockerfile --tag "${TAG}" --label "runnumber=${GITHUB_RUN_ID}"
+docker run --workdir /github/workspace --rm -e GODOT_VERSION -e REPO_NAME -e INPUT_PROJECT-DIRECTORY -e INPUT_EXPORT-DEBUG -e INPUT_EXPORT-PACK -e INPUT_EXPORT-PLATFORM -e INPUT_PLATFORM -e INPUT_EXECUTABLE-NAME -e HOME -e GITHUB_JOB -e GITHUB_REF -e GITHUB_SHA -e GITHUB_REPOSITORY -e GITHUB_REPOSITORY_OWNER -e GITHUB_RUN_ID -e GITHUB_RUN_NUMBER -e GITHUB_RETENTION_DAYS -e GITHUB_RUN_ATTEMPT -e GITHUB_ACTOR -e GITHUB_WORKFLOW -e GITHUB_HEAD_REF -e GITHUB_BASE_REF -e GITHUB_EVENT_NAME -e GITHUB_SERVER_URL -e GITHUB_API_URL -e GITHUB_GRAPHQL_URL -e GITHUB_REF_NAME -e GITHUB_REF_PROTECTED -e GITHUB_REF_TYPE -e GITHUB_WORKSPACE -e GITHUB_ACTION -e GITHUB_EVENT_PATH -e GITHUB_ACTION_REPOSITORY -e GITHUB_ACTION_REF -e GITHUB_PATH -e GITHUB_ENV -e RUNNER_OS -e RUNNER_ARCH -e RUNNER_NAME -e RUNNER_TOOL_CACHE -e RUNNER_TEMP -e RUNNER_WORKSPACE -e ACTIONS_RUNTIME_URL -e ACTIONS_RUNTIME_TOKEN -e ACTIONS_CACHE_URL -e GITHUB_ACTIONS=true -e CI=true -v "/home/runner/work/_temp/_github_home":"/github/home" -v "/home/runner/work/_temp/_github_workflow":"/github/workflow" -v "/home/runner/work/_temp/_runner_file_commands":"/github/file_commands" -v "/home/runner/work/godot-github-ci-actions/godot-github-ci-actions":"/github/workspace" "$PROJECT_DIRECOTRY" "$EXPORT_DEBUG" "$EXPORT_PACK" "$EXPORT_PLATFORM" "$PLATFORM" $EXECUTABLE_NAME"
+cd .. 
